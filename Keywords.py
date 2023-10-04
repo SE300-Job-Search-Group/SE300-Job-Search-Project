b0eab4b90bc2068b8c82056c6d6760b3682a8975
graceeliza967 <BurnsG4@my.erau.edu>
2023-10-04 16:30:56 -0400

    keyword outline

---
+++ Keywords.py
@@ -0,0 +1,13 @@
+#Here's where we can extract keywords from the employer reviews
+
+#import
+
+#parse words
+
+#create dictionary
+
+#remove common words from list
+
+#count frequency of words + sort
+
+#show top 10?
