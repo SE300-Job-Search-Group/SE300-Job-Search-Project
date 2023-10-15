03daf72577755c431fbc46ef447ab8d9b73fdaad
sungjepark <92417859+sungjepark@users.noreply.github.com>
2023-10-14 20:51:49 -0400

    Created job, user, and company Objects

---
+++ src/Objects/job.py
@@ -0,0 +1,12 @@
+import user
+import sqlite3
+
+class job:
+    def __init__(self,id):
+        self.id = id
+        
+    def getId(self):
+        return self.id
+    
+    def userMatch(self,usr: user) -> float:
+        return 1 #should return match ranking 
--- tools/createFakeJobDatabase.py
+++ tools/createFakeJobDatabase.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 db = sqlite3.connect("./database/test.db")
-
 dbctrl = db.cursor()
 
+# yea idk i just wanted to test it out :)
 dbctrl.execute("""
 INSERT INTO jobs VALUES
 (1,'Entry Level Software Engineer', 'PathFinder', 'Daytona Beach','FL','idk')
