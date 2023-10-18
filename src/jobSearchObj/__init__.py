62a3abbdb516be5af72e30ce57faf7b2c63f3f27
sungjepark <92417859+sungjepark@users.noreply.github.com>
2023-10-18 10:58:06 -0400

    Misc. Changes to Package & Data Structures

---
+++ src/jobSearchObj/__init__.py
@@ -0,0 +1,4 @@
+from .job import Job
+from .company import Company
+from .user import User
+from .jobHandler import JobHandler
--- src/jobSearchObj/companyTest.py
+++ src/jobSearchObj/companyTest.py
@@ -2,5 +2,8 @@ from company import Company
 
 #example case of how to use job Object
 tempComp = Company(1) # ID = 1
-print(tempComp.getId())
-print(tempComp.getName())
+print("Company ID: ", tempComp.getId())
+print("Company Name: ", tempComp.getName())
+print("Industry: ", tempComp.getIndustry())
+print("Keywords: ", tempComp.getKeywords())
+print("Rating: ", tempComp.getRating())
