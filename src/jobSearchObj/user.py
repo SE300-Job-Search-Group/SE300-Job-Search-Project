7f48a064131843261c78e46755fca50404baa912
graceeliza967 <BurnsG4@my.erau.edu>
2023-10-18 15:51:54 -0400

    updates to user

--- src/jobSearchObj/user.py
+++ src/jobSearchObj/user.py
@@ -11,6 +11,9 @@ class User:
         #fetch user data
         dbctrl = db.cursor()
 
+        tempDB=dbctrl.execute('SELECT * FROM user WHERE user_id ='+str(id))
+        tempData = tempDB.fetchone()
+
         # setting attributes
         self._username = tempData[1]
         self._password = tempData[2]
@@ -20,5 +23,27 @@ class User:
         self.state = tempData[6]
         self.minSalary = tempData[7]
         self.maxSalary = tempData[8]
+
+    # functions
+
+    def getId(self):
+        return self.id
+    
+    def getKeywords(self):
+        return self.keywords
+    
+    def getSkills(self):
+        return self.skills
+    
+    def getCity(self):
+        return self.city
+    
+    def getState(self):
+        return self.state
+    
+    def getSalaryRange(self):
+        return [self.minSalary, self.maxSalary]
+
+
 
 
