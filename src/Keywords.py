0049008c60cc4cd08148c46c651deaacb6efc345
graceeliza967 <BurnsG4@my.erau.edu>
2023-11-05 14:56:56 -0500

    company match and keywords

--- src/Keywords.py
+++ src/Keywords.py
@@ -1,13 +1,33 @@
+from jobSearchObj import GenericDatabaseHandler
+
 #Here's where we can extract keywords from the employer reviews
 
 #import
+dbh = GenericDatabaseHandler("./database/test.db")
+initial = dbh.getTable("keywords")
+print(initial)
 
 #parse words
+Keywords = []
+Keywords = initial.split()
+dbh.close()
 
 #create dictionary
+Dictionary = dict()
 
 #remove common words from list
+criteria = [ e for e in Keywords if len(e) > 4] #this just filters out short words
+# will try a criteria that filters out any articles, maybe there's a library I can use
 
-#count frequency of words + sort
+#count frequency of words
+for x in criteria:
+    if x in Dictionary:
+        Dictionary[x] = Dictionary.get(x) + 1
+    else:
+        Dictionary[x] = 1
 
-#show top 10?
+#show top 10 most frequent- maybe we can switch to storing top ten somewhere?
+TopWords = sorted(Dictionary.items(), key=lambda x:x[1], reverse=True)
+TopSorted= dict(TopWords)
+TopTen=TopWords[:10]
+print(*TopTen, sep = "\n") 
---
+++ src/jobSearchObj/companyMatch.py
@@ -0,0 +1,21 @@
+# loop to compare user keywords from profile to keywords from company reviews
+def keywordCompare():
+    usrKeywords = 1 #import user keywords stored in SQL
+    numMatches = 1 #placeholder
+    return numMatches, usrKeywords
+
+# import user's preference for category weight
+normRank = 1 #placeholder for normalized user ranking
+# import company scores
+catScores = [1, 2, 3, 4, 5]
+# normalize rankings
+
+# loop through each category rating
+catRanking = []
+for cat in catScores:
+    catRanking = (normRank *cat)/5
+
+# score company
+def scoreCompany():
+    keywordCompare()
+    Score = (numMatches/usrKeywords) + catRating
