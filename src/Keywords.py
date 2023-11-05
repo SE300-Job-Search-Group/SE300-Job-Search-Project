db81fd6bc01ef60a2d5bc4924ca89334a0b3a089
graceeliza967 <BurnsG4@my.erau.edu>
2023-11-05 16:00:42 -0500

    keywords
    you may need to pip install rake-nltk
    
    python -c "import nltk; nltk.download('stopwords')"
    
    python -c "import nltk; nltk.download('punkt')"

--- src/Keywords.py
+++ src/Keywords.py
@@ -1,33 +1,13 @@
-from jobSearchObj import GenericDatabaseHandler
-
+from rake_nltk import Rake
+import rake_nltk
 #Here's where we can extract keywords from the employer reviews
 
 #import
-dbh = GenericDatabaseHandler("./database/test.db")
-initial = dbh.getTable("keywords")
+initial = "This is a test until we can successfully import the company reviews from the SQL database."
 print(initial)
 
 #parse words
-Keywords = []
-Keywords = initial.split()
-dbh.close()
-
-#create dictionary
-Dictionary = dict()
-
-#remove common words from list
-criteria = [ e for e in Keywords if len(e) > 4] #this just filters out short words
-# will try a criteria that filters out any articles, maybe there's a library I can use
-
-#count frequency of words
-for x in criteria:
-    if x in Dictionary:
-        Dictionary[x] = Dictionary.get(x) + 1
-    else:
-        Dictionary[x] = 1
-
-#show top 10 most frequent- maybe we can switch to storing top ten somewhere?
-TopWords = sorted(Dictionary.items(), key=lambda x:x[1], reverse=True)
-TopSorted= dict(TopWords)
-TopTen=TopWords[:10]
-print(*TopTen, sep = "\n") 
+r = Rake()
+r.extract_keywords_from_text(initial)
+rPhrase = r.get_ranked_phrases()
+print(rPhrase)
