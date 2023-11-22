d38a311151bf87f6e8fea8965bfe1010abd793fa
sungjepark <sungje.park123@gmail.com>
2023-11-21 21:03:37 -0500

    Update __init__.py

--- src/jobSearchObj/__init__.py
+++ src/jobSearchObj/__init__.py
@@ -6,6 +6,9 @@ from .company import Company
 from .user import User
 from .jobHandler import JobHandler
 from .userHandler import UserHandler
+from.jobMatch import JobMatch
+from .companyMatch import CompanyMatch
 from .words import GenericWord, Tag, Keyword, Skill
 from .location import Location
+
 from.databaseHandler import GenericDatabaseHandler,WordsDBHandler,UserDBHandler,JobDBHandler,CompanyDBHandler
