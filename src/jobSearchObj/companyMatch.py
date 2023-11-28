420dbfd7f36c06c984c5826387199e14906020f9
graceeliza967 <BurnsG4@my.erau.edu>
2023-11-28 17:12:50 -0500

    update

--- src/jobSearchObj/companyMatch.py
+++ src/jobSearchObj/companyMatch.py
@@ -54,6 +54,6 @@ class CompanyMatch:
         return matches
         
 # test:
-cm = CompanyMatch()
-matches = cm.scoreCompany("Northrop Grumman", "Lockheed Martin", 1, 3, 5, 7, 9)
-print(matches)
+#cm = CompanyMatch()
+#matches = cm.scoreCompany("Northrop Grumman", "Lockheed Martin", 1, 3, 5, 7, 9)
+#print(matches)
